3adad2b318d789241bec8759c01a4558876cc849
Nitika2334 <nitika2334.be21@chitkara.edu.in>
2024-02-23 02:01:19 +0530

    added components(menu bar and cart) to the header and added the footer

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,16 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer/>
+      
     </BrowserRouter>
   );
 };
--- Frontend/src/components/header/Header.js
+++ Frontend/src/components/header/Header.js
@@ -1,52 +1,90 @@
-import React from 'react'
-import styles from "./Header.module.scss"
-import {Link, NavLink} from "react-router-dom"
+import React, { useState } from 'react';
+import styles from "./Header.module.scss";
+import { Link, NavLink } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { FaTimes } from 'react-icons/fa';
 
-export const logo =(
+export const Logo = () => ( 
   <div className={styles.logo}>
     <Link to='/'>
       <h2>
         Prak<span>riti</span>
       </h2>
     </Link>
-
   </div>
-)
+);
 
-const activeLink = (isActive) => (isActive ? `${styles.active}` : "")
+const activeLink = (isActive) => (isActive ? `${styles.active}` : "");
 
-const header = () => {
+const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  }
+
+  const hideMenu = () => {
+    setShowMenu(false);
+  }
+
+  const cart = (
+    <span className={styles.cart}>
+      <Link to="/cart">
+        Cart
+        <FaShoppingCart size={20} />
+        <p>0</p>
+      </Link>
+    </span>
+  );
+
   return (
     <header>
       <div className={styles.header}>
-        {logo}
-        <nav>
+        <Logo />
+        
+        <nav className={ showMenu ? `${styles["show-nav"]}`: `${styles["hide-nav"]}`}>
+
+            <div className={ showMenu ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`: `${styles["nav-wrapper"]}`} onClick={hideMenu}>
+
+            </div>
 
           <ul>
+            <li className={styles["logo-mobile"]}>
+            <Logo />
+                <FaTimes size={22} color="#fff" onClick={hideMenu} />
+
+            
+
+            </li>
+
             <li>
               <NavLink to="/shop" className={activeLink}>
                 Shop
               </NavLink>
             </li>
           </ul>
-
           <div className={styles['header-right']}>
             <span className={styles.links}>
-              <NavLink to={"login"} className={activeLink}>
+              <NavLink to={"login"} className={activeLink(true)}>
                 Login
               </NavLink>
-              <NavLink to={"register"} className={activeLink}>
+              <NavLink to={"register"} className={activeLink(true)}>
                 Register 
               </NavLink>
-              <NavLink to={"order-history"} className={activeLink}>
+              <NavLink to={"order-history"} className={activeLink(true)}>
                 My Order
               </NavLink>
             </span>
+            {cart}
           </div>
         </nav>
+        <div className={styles['menu-icon']}>
+            {cart}
+          <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default header
+export default Header;

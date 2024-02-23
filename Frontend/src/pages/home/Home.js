9ba09f5e06ba07aee74fd52a0508348a2fcddf36
Nitika2334 <nitika2334.be21@chitkara.edu.in>
2024-02-23 23:54:56 +0530

    added homeInfoBox and the styles of the home page

--- Frontend/src/pages/home/Home.js
+++ Frontend/src/pages/home/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Slider from "../../components/slider/Slider"
+import HomeInfoBox from './HomeInfoBox';
 
 const home = props => {
   return (
     <>
     <Slider/>
+    <section>
+      <div className="conatiner">
+        <HomeInfoBox/>
+
+      </div>
+
+    </section>
     </>
   )
 }
---
+++ Frontend/src/pages/home/HomeInfoBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeInfoBox = () => {
+  return (
+    <div>
+      HomeInfoBox
+    </div>
+  )
+}
+
+export default HomeInfoBox

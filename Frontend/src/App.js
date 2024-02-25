ad4d102e0b3fa699ee14ef1b7bff38c9859b78e0
IshikaBaliyan <ishikabaliyan2019@gmail.com>
2024-02-26 02:11:51 +0530

    creted the login page and did some designing on it

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import{Spinner} from "./components/loader/Loader"
+import Login from "./pages/home/auth/Login";
+import Register from "./pages/home/auth/Register";
+
 
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Spinner/>
+    
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       <Footer/>
       
---
+++ Frontend/src/pages/home/auth/Login.js
@@ -0,0 +1,55 @@
+import React ,{useState} from "react"
+
+import loginImg from "../../../assets/login.png"
+import {Link} from "react-router-dom"
+import Card from "../../../components/card/Card"
+import styles from "./auth.module.scss"
+
+const Login = () => {
+    const [email, setEmail ] = useState("")
+    const[password,setPassword] = useState("")
+    const loginUser=()=>{} 
+  return (
+    <section className={`container ${styles.auth}`}>
+    <div className={styles.img}>
+        <img src={loginImg} alt = "Login" width ="400"/>
+
+    </div>
+    
+    <Card>
+  
+    <div className={styles.form}>
+        <h2>Login</h2>
+        <form onSubmit = {loginUser}>
+       <input 
+              type = "text"
+              placeholder = "Email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              />
+        <input
+              type = "password"
+              placeholder='Password'  
+              required value = {password}
+              onChange={(e)=>setPassword(e.target.value)}
+              /> 
+              <button type = "submit" className='--btn --btn-primary --btn-block'>
+                login
+              </button>
+              </form>
+              <span className={styles.register}>
+                <p>Don't have an account?</p>
+                <Link to="/register">Register</Link>
+              </span>
+
+    </div>
+    </Card>
+  </section>
+  )
+
+    
+  
+ }
+
+export default Login

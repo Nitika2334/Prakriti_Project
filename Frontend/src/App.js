a5958312965545d054f278db07fd6cf0ee0b42cb
IshikaBaliyan <ishikabaliyan2019@gmail.com>
2024-02-26 00:51:33 +0530

    created the loader and spinner for enhancing the ui

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,11 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import{Spinner} from "./components/loader/Loader"
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
+      <Spinner/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>

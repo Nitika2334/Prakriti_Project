9e2f9fa83a54eaab922662fde132e1b2e769bff1
Nitika2334 <nitika2334.be21@chitkara.edu.in>
2024-02-25 19:55:50 +0530

    added login to the backend

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -37,8 +37,15 @@ export const registerUser = asyncHandler(async (req, res) => {
     const user = new User({
         name,
         email,
-        password,
+        password
     });
+
+    try {
+        await user.save();
+
+    } catch (error) {
+        console.log(error);
+    }
 
     // Generate token
     const token = generateToken(user._id);
@@ -69,9 +76,67 @@ export const registerUser = asyncHandler(async (req, res) => {
 
 
 //login user
-export const loginUser=asyncHandler(async(req,res)=>{
-    res.send("login user.....")
-});
+export const loginUser = async(req,res,next) => {
+    const {email, password } = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404,"user not found"));
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHandler(401,"wrong credentials"));
+        const token = jwt.sign({id: validUser._id}, `${process.env.JWT_SECRET}`);
+        const rest={...validUser._doc};
+        delete rest.password;
+        res
+        .cookie('access_token',token,{httpOnly: true})
+        .status(200)
+        .json(rest)
+    }catch(error){
+        next(error)
+    }
+};
+
+const errorHandler=(statusCode, message)=>{
+    const error=new Error();
+    error.statusCode=statusCode;
+    error.message=message;
+    return error;
+};
+
+// export const loginUser=asyncHandler(async(req,res)=>{
+//     const {email,password} =req.body;
+
+//     //Validate Request
+//     if(!email || !password){
+//         res.status(400);throw new Error("Please add email and password")
+//     }
+
+//     //check if user exists
+//     const userE = await User.findOne({ email });
+
+//     //user password, check if the password is correct
+//     const passwordIsCorrect= bcryptjs.compareSync(password,User.password)
+
+//     //Generate token
+//     const token = generateToken(userE._id);
+//     const newUser = await User.findOne({ email }).select("-password");
+//     if(userE && passwordIsCorrect){
+//         res.cookie("token", token, {
+//             path: "/",
+//             httpOnly: true,
+//             expires: new Date(Date.now() + 1000 * 86400),
+//             // secure:true,
+//             // samesite:none,
+//         });
+
+//         // Send user data
+//         res.status(201).json(newUser);
+//     }
+//     else{
+//         res.status(400);
+//         throw new Error("Invalid email or password");
+//     }
+
+// });
 
 
 // export const registerUser = async (req,res,next) =>{

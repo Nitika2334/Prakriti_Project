abd73213cd6e667ff1f979ce3c0677771c7bcbc1
IshikaBaliyan <ishikabaliyan2019@gmail.com>
2024-02-28 01:45:37 +0530

    added get user, update user logic

--- Frontend/src/redux/features/auth/authService.js
+++ Frontend/src/redux/features/auth/authService.js
@@ -29,11 +29,32 @@ const getLoginStatus = async () => {
   return response.data;
 };
 
+// Get user
+const getUser = async () => {
+  const response = await axios.get(API_URL + "getUser");
+  return response.data;
+};
+
+// Get update user
+const updateUser = async (userData) => {
+  const response = await axios.patch(API_URL + "updateUser" ,userData);
+  return response.data;
+};
+
+// Get update photo
+const updatePhoto = async (userData) => {
+  const response = await axios.patch(API_URL + "updatePhoto" ,userData);
+  return response.data;
+};
+
 const authService = {
   register,
   login,
   logout,
   getLoginStatus,
+  getUser,
+  updateUser,
+  updatePhoto
 };
 
 export default authService;
--- Frontend/src/redux/features/auth/authSlice.js
+++ Frontend/src/redux/features/auth/authSlice.js
@@ -69,6 +69,45 @@ export const getLoginStatus=createAsyncThunk("auth/getLoginStatus",async (_,thun
 }
 );
 
+//get user
+export const getUser=createAsyncThunk("auth/getUser",async (_,thunkAPI) => {
+  try {
+    return await authService.getUser();
+  } catch (error) {
+    const message=(error.response && error.response.data && error.response.data.message) 
+    || error.message
+    || error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+}
+);
+
+//update user
+export const updateUser=createAsyncThunk("auth/updateUser",async (userData,thunkAPI) => {
+  try {
+    return await authService.updateUser(userData);
+  } catch (error) {
+    const message=(error.response && error.response.data && error.response.data.message) 
+    || error.message
+    || error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+}
+);
+
+//update photo
+export const updatePhoto=createAsyncThunk("auth/updatePhoto",async (userData,thunkAPI) => {
+  try {
+    return await authService.updatePhoto(userData);
+  } catch (error) {
+    const message=(error.response && error.response.data && error.response.data.message) 
+    || error.message
+    || error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+}
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -164,6 +203,66 @@ const authSlice = createSlice({
         state.isLoading= false;
         state.isError= true;
         state.message=action.payload;
+      })
+
+
+      //getUser
+      
+      .addCase(getUser.pending , (state)=>{
+        state.isLoading= true;
+      })
+      .addCase(getUser.fulfilled,(state,action)=>{
+        state.isLoading= false;
+        state.isSuccess= true;
+        state.isLoggedIn=true;
+        state.user=action.payload;
+        console.log(action.payload);
+      })
+      .addCase(getUser.rejected,(state,action )=>{
+        state.isLoading= false;
+        state.isError= true;
+        state.message=action.payload;
+        toast.error(action.payload);
+      })
+
+      //updateUser
+      
+      .addCase(updateUser.pending , (state)=>{
+        state.isLoading= true;
+      })
+      .addCase(updateUser.fulfilled,(state,action)=>{
+        state.isLoading= false;
+        state.isSuccess= true;
+        state.isLoggedIn=true;
+        state.user=action.payload;
+        toast.success("User updated")
+        console.log(action.payload);
+      })
+      .addCase(updateUser.rejected,(state,action )=>{
+        state.isLoading= false;
+        state.isError= true;
+        state.message=action.payload;
+        toast.error(action.payload);
+      })
+
+      //updatePhoto
+      
+      .addCase(updatePhoto.pending , (state)=>{
+        state.isLoading= true;
+      })
+      .addCase(updatePhoto.fulfilled,(state,action)=>{
+        state.isLoading= false;
+        state.isSuccess= true;
+        state.isLoggedIn=true;
+        state.user=action.payload;
+        toast.success("User photo updated")
+        console.log(action.payload);
+      })
+      .addCase(updatePhoto.rejected,(state,action )=>{
+        state.isLoading= false;
+        state.isError= true;
+        state.message=action.payload;
+        toast.error(action.payload);
       })
 
   }

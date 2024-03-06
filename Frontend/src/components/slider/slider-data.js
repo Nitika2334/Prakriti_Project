87eea2d3e140b91c04e0bdef33419bb516495b7b
IshikaBaliyan <ishikabaliyan2019@gmail.com>
2024-03-06 23:17:04 +0530

    made changes to slider-data.js

--- Frontend/src/components/slider/slider-data.js
+++ Frontend/src/components/slider/slider-data.js
@@ -4,26 +4,26 @@ export const sliderData = [
   {
     image: "https://images.unsplash.com/photo-1694478712030-66281d7d3723?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGhlcmJzJTIwYW5kJTIwc2hlcmJzfGVufDB8fDB8fHww",
     heading: "Herbs and Shurbs",
-    desc: "Up to 30% off on all onsale proucts.",
+    
   },
   {
     image: "https://images.unsplash.com/photo-1636045466232-539c7bd7817e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YXFhdXRpYyUyMHBsYW50c3xlbnwwfHwwfHx8MA%3D%3D",
     heading: "Aquatic plants ",
-    desc: "Up to 30% off on all onsale proucts.",
+    
   },
   {
     image: "https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830_1280.jpg",
     heading: "Flowering Plants",
-    desc: "Up to 30% off on all onsale proucts.",
+    
   },
   {
     image: "https://cdn.pixabay.com/photo/2016/06/14/16/19/carnivorous-plant-1456932_1280.jpg",
     heading: "Carnivoras plants",
-    desc: "Up to 30% off on all onsale proucts.",
+    
   },
   {
     image: "https://cdn.pixabay.com/photo/2016/11/30/15/00/apples-1872997_1280.jpg",
     heading: "Fruitful plants",
-    desc: "Up to 30% off on all onsale proucts.",
+   
   },
 ];
